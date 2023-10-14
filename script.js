5a153588b85d65a940204edbbe9740b9b5da500b
Dinesh S <dineshsaravan2114@gmail.com>
2023-10-14 21:16:02 +0530

    initial and final product

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let count = 0;
 
 function increment(){
-    count = count +1;
+    count +=1;
     document.getElementById("count").innerText = count;
 }
 
 function decrement(){
-    count = count -1;
+    count -=1;
     if(count <= 0){
     count = 0;
     }
 document.getElementById("count").innerText = count;
 }
 
-function reset(){
+// function reset(){
+// }
+
+function save(){
+    // console.log(count);
+    let output = document.getElementById("saved");
+    
+    output.textContent+= " - " + count ;
     document.getElementById("count").innerText = 0;
+    count = 0;
 }
