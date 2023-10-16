dd3df7b2464e5f039842f998761e408b99d974fa
Dinesh S <dineshsaravan2114@gmail.com>
2023-10-16 21:04:38 +0530

    display p1 and p2

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ let count = 0;
 
 function increment(){
     count +=1;
+    let output = document.getElementById("p2");
+    if(output.style.display === "block"){
+        output.style.display="none";
+    }
+    let outputp1=document.getElementById("p1");
+    if(outputp1.style.display === "none"){
+        outputp1.style.display = "block";
+    }
     document.getElementById("count").innerText = count;
+    // let output_ = document.getElementById("p1");
+    // output_.innerText = "Last Entries are as Folows";
 }
 
 function decrement(){
@@ -18,9 +28,22 @@ document.getElementById("count").innerText = count;
 
 function save(){
     // console.log(count);
-    let output = document.getElementById("saved");
+    let output = document.getElementById("p1");
     
     output.textContent+= " - " + count ;
     document.getElementById("count").innerText = 0;
     count = 0;
+}
+
+function reset(){
+    let output = document.getElementById("p2");
+    if(output.style.display === "none"){
+        output.style.display="block";
+    }
+    let outputp1=document.getElementById("p1");
+    if(outputp1.style.display === "block"){
+        outputp1.style.display = "none";
+    }
+    // output.innerText = "Last Entries were reset"
+
 }
